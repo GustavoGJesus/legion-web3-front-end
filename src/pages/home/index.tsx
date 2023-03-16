49b4ccf7b8fbf18cd4496f72882316f4dbf01aad
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-03-16 19:47:17 -0300

    feat: create home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/header";
+import { Main } from "../../components/main";
+
+export function Home() {
+    return(
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
