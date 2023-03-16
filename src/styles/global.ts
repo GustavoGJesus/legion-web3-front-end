7d590815975bffd37f49a86443494fb3b035d754
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-03-16 19:46:41 -0300

    feat: create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    /* :root{
+        |#5B2B76
+        #000000
+    } */
+
+    *{
+        margin: 0;
+        padding: 0;
+
+        font-family: 'Poppins', sans-serif;
+
+        /* background: #000000; */
+        color: #F0F0F0;
+    }
+
+    body{
+         background: #000000;
+    }
+
+`;
