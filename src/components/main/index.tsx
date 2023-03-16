5500e4764acec7f0d2dcc19bfb689a1368971e4d
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-03-16 19:48:17 -0300

    feat: create main section

---
+++ src/components/main/index.tsx
@@ -0,0 +1,45 @@
+import { ContainerCryptos, ContentCyptos, Description, MainContent, MainWarraper, Paragraph, Title } from "./styles";
+
+import EthIcon from '../../assets/ethereum.png';
+import PolygonIcon from "../../assets/polygon.png";
+import SolanaIcon from "../../assets/solana.png"
+import { DevelopmentProcess } from "../development-process";
+import { ContactForm } from "../form-contact";
+
+export function Main(){
+    return (
+      <MainWarraper>
+        <MainContent>
+          <Title>
+            Welcome to Legion, let's put your idea into practice?
+          </Title>
+          <Description>
+            Specialized web3 design for decentralized idea
+          </Description>
+
+          <ContainerCryptos>
+            <ContentCyptos>
+              <img src={EthIcon} alt="logo" style={{ width: "50px" }} />
+              <Paragraph>Ethereum</Paragraph>
+            </ContentCyptos>
+
+            <ContentCyptos>
+              <img src={PolygonIcon} alt="logo" style={{ width: "50px" }} />
+              <Paragraph>Polygon</Paragraph>
+            </ContentCyptos>
+
+            <ContentCyptos>
+              <img src={SolanaIcon} alt="logo" style={{ width: "50px" }} />
+              <Paragraph>Solana</Paragraph>
+            </ContentCyptos>
+          </ContainerCryptos>
+
+          <div>
+            <DevelopmentProcess />
+          </div>
+
+          <ContactForm />
+        </MainContent>
+      </MainWarraper>
+    );
+}
---
+++ src/components/main/styles.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const MainWarraper = styled.div`
+    padding: 90px 50px;
+`;
+
+export const MainContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    /* width: 70%; */
+`;
+
+export const Title = styled.p`
+    font-size: 18px;
+    font-weight: 200;
+`;
+
+export const Description = styled.h2`
+    background-image: linear-gradient(#fff, #171717);
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+    font-size: 70px;
+`;
+
+export const ContainerCryptos = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 80px;
+    align-items: center;
+    gap: 90px
+`;
+
+export const ContentCyptos = styled.div`
+    display: flex;
+    flex-direction: row; 
+    gap: 10px
+`;
+
+export const Paragraph = styled.p`
+    font-size: 30px;
+    font-weight: 200
+`;
