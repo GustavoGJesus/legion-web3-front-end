e36e3cbf33fbb925acc1c76995bfec2d8a9a20f5
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-03-17 16:47:44 -0300

    feat: add animations

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -4,6 +4,8 @@ import { ReactNode } from "react";
 // styles
 import { CardContent, CardWrapper, Title } from "./styles";
 
+import { motion } from "framer-motion";
+
 // types props
 interface CardsProps{
   title: string;
@@ -12,12 +14,18 @@ interface CardsProps{
 
 export function Cards({ title, children }: CardsProps) {
   return (
-      <CardWrapper>
-        <CardContent>
-          <Title>{title}</Title>
+    <>
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}>
+        <CardWrapper>
+          <CardContent>
+            <Title>{title}</Title>
 
-        {children}
-        </CardContent>
-      </CardWrapper>
+            {children}
+          </CardContent>
+        </CardWrapper>
+      </motion.div>
+    </>
   );
 }
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -5,41 +5,48 @@ import PolygonIcon from "../../assets/polygon.png";
 import SolanaIcon from "../../assets/solana.png"
 import { DevelopmentProcess } from "../development-process";
 import { ContactForm } from "../form-contact";
+import { motion } from "framer-motion";
 
 export function Main(){
     return (
       <MainWarraper>
-        <MainContent>
-          <Title>
-            Welcome to Legion, let's put your idea into practice?
-          </Title>
-          <Description>
-            Specialized web3 design for decentralized idea
-          </Description>
+          <MainContent>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1.2 }}
+            >
+              <Title>
+                Welcome to Legion, let's put your idea into practice?
+              </Title>
+              <Description>
+                Specialized web3 design for decentralized idea
+              </Description>
+            </motion.div>
 
-          <ContainerCryptos>
-            <ContentCyptos>
-              <img src={EthIcon} alt="logo" style={{ width: "50px" }} />
-              <Paragraph>Ethereum</Paragraph>
-            </ContentCyptos>
+            <ContainerCryptos>
+              <ContentCyptos>
+                <img src={EthIcon} alt="logo" style={{ width: "50px" }} />
+                <Paragraph>Ethereum</Paragraph>
+              </ContentCyptos>
 
-            <ContentCyptos>
-              <img src={PolygonIcon} alt="logo" style={{ width: "50px" }} />
-              <Paragraph>Polygon</Paragraph>
-            </ContentCyptos>
+              <ContentCyptos>
+                <img src={PolygonIcon} alt="logo" style={{ width: "50px" }} />
+                <Paragraph>Polygon</Paragraph>
+              </ContentCyptos>
 
-            <ContentCyptos>
-              <img src={SolanaIcon} alt="logo" style={{ width: "50px" }} />
-              <Paragraph>Solana</Paragraph>
-            </ContentCyptos>
-          </ContainerCryptos>
+              <ContentCyptos>
+                <img src={SolanaIcon} alt="logo" style={{ width: "50px" }} />
+                <Paragraph>Solana</Paragraph>
+              </ContentCyptos>
+            </ContainerCryptos>
 
-          <ContainerDevelopmentProcess>
-            <DevelopmentProcess />
-          </ContainerDevelopmentProcess>
+            <ContainerDevelopmentProcess>
+              <DevelopmentProcess />
+            </ContainerDevelopmentProcess>
 
-          <ContactForm />
-        </MainContent>
+            <ContactForm />
+          </MainContent>
       </MainWarraper>
     );
 }
